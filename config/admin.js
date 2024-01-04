f18876659d048092e76dad18498297d38430d77e
M-Metaw3 <mizoomizoo161@gmail.com>
2024-01-04 08:57:12 +0200

    done

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,10 @@
+const crypto = require('crypto');
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: crypto.randomBytes(16).toString('base64'),
   },
   transfer: {
     token: {
